36598f30a832806132f09b54bc542f989b19bb56
Wenzheng <vincent.jin6@icloud.com>
2022-09-25 16:57:25 +1000

    map finished

--- Assets/Scripts/levelLayout.cs
+++ Assets/Scripts/levelLayout.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class levelLayout : MonoBehaviour
 {
 
+
     private int[,] map = {
             {1,2,2,2,2,2,2,2,2,2,2,2,2,7}, 
             {2,5,5,5,5,5,5,5,5,5,5,5,5,4}, 
@@ -23,6 +24,7 @@ public class levelLayout : MonoBehaviour
             {0,0,0,0,0,0,5,0,0,0,4,0,0,0}, 
         };
 
+    [SerializeField] private GameObject topLeftQuadrant;
     [SerializeField] private GameObject insideWall;
     [SerializeField] private GameObject insideCorner;
     [SerializeField] private GameObject outsideWall;
@@ -45,6 +47,12 @@ public class levelLayout : MonoBehaviour
     void Start()
     {
         Initialize();
+        GameObject topRightQuadrant = Instantiate(topLeftQuadrant, Vector3.zero, Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)), this.transform);
+        topRightQuadrant.transform.position = new Vector3(columns+1, 0, 0);
+        GameObject bottomLeftQuadrant = Instantiate(topLeftQuadrant, Vector3.zero, Quaternion.Euler(new Vector3(180.0f, 0.0f, 0.0f)), this.transform);
+        bottomLeftQuadrant.transform.position = new Vector3(0, -rows-1, 0);
+        GameObject bottomRightQuadrant = Instantiate(topLeftQuadrant, Vector3.zero, Quaternion.Euler(new Vector3(0.0f, 0.0f, 180.0f)), this.transform);
+        bottomRightQuadrant.transform.position = new Vector3(columns+1, -rows-1, 0);
     }
 
     private void Initialize(){
@@ -55,7 +63,7 @@ public class levelLayout : MonoBehaviour
                 switch (map[i,j])
                 {
                     case 1:
-                        ele = Instantiate(outsideCorner, position, Quaternion.identity, this.transform);
+                        ele = Instantiate(outsideCorner, position, Quaternion.identity, this.topLeftQuadrant.transform);
                         if(j == 0 && i == 0){
                             ele.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f));
                         }
@@ -65,7 +73,7 @@ public class levelLayout : MonoBehaviour
                         
                         break;
                     case 2:
-                        ele = Instantiate(outsideWall, position, Quaternion.identity, this.transform);
+                        ele = Instantiate(outsideWall, position, Quaternion.identity, this.topLeftQuadrant.transform);
                         if(i > 0 && i < rows && map[i-1,j] != 0 && j == 0){
                             ele.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 90.0f));
                         }
@@ -78,7 +86,7 @@ public class levelLayout : MonoBehaviour
                         
                         break;
                     case 3:
-                        ele = Instantiate(insideCorner, position, Quaternion.identity, this.transform);
+                        ele = Instantiate(insideCorner, position, Quaternion.identity, this.topLeftQuadrant.transform);
                         if(i > 0 && i < rows && map[i-1,j] == 2 && map[i,j-1] == 2){
                             ele.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 180.0f));
                         }else if(i > 0 && i < rows && map[i,j-1] == 2){
@@ -104,7 +112,7 @@ public class levelLayout : MonoBehaviour
                         }
                         break;
                     case 4:
-                        ele = Instantiate(insideWall, position, Quaternion.identity, this.transform);
+                        ele = Instantiate(insideWall, position, Quaternion.identity, this.topLeftQuadrant.transform);
                         if(j == columns-1 && (map[i-1,j] == 7 || map[i-1,j] == 3 || map[i-1,j] == 4)){
                             ele.transform.rotation = Quaternion.Euler(new Vector3(0.0f,0.0f,-90.0f));
                         }
@@ -119,16 +127,9 @@ public class levelLayout : MonoBehaviour
                         }else if(j < columns-1 && (map[i-1,j] == 3 || map[i-1,j] == 4) && (map[i,j-1] == 0 || map[i,j-1]== 5 || map[i,j-1] ==6)){
                             ele.transform.rotation = Quaternion.Euler(new Vector3(0.0f,0.0f,90.0f));
                         }
-                        // if(j >0 && j < columns-1 && map[i,j-1] == 4 && map[i,j+1] == 4){
-                        //     ele.transform.rotation = Quaternion.Euler(Vector3.zero);
-                        // }else if((j >0 && j < columns-1 && map[i, j-1] == 3 && map[i,j+1] == 4) || (j >0 && j < columns-1 && map[i,j-1] == 4 && map[i,j+1] ==3)) {
-                        //     ele.transform.rotation = Quaternion.Euler(Vector3.zero);
-                        // }else if(map[i,j-1] != 5 && map[i,j-1] != 6){
-                        //     ele.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, -90.0f));
-                        // }
                         break;
                     case 7:
-                        ele = Instantiate(junctionWall, position, Quaternion.identity, this.transform);
+                        ele = Instantiate(junctionWall, position, Quaternion.identity, this.topLeftQuadrant.transform);
                         break;
 
 
